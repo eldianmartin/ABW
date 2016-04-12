c61896b5a2fb451f9ca5c842a25640e81bb0041d
eldianmartin <martineldian@gmail.com>
2016-04-12 09:45:13 +0700

    starting seo

--- js/controller/homeCtrl.js
+++ js/controller/homeCtrl.js
@@ -1,7 +1,7 @@
 ﻿app.controller('homeCtrl', function ($scope) {
       $scope.slides = [
-        { image: 'images/banner/banner1.jpg', id: 0,title: "Cocok untuk semua orang", description: "Diy diamond painting cocok untuk semua usia dari 5- 75 tahun"  },
-        { image: 'images/banner/banner2.jpg', id: 1, title: "Motif baru", description: "New arrival tanggal 4 April 2014" },
+        { image: 'images/banner/banner1.jpg', id: 0,url: "#/Test"  },
+        { image: 'images/banner/banner2.jpg', id: 1, url: "#/Test" },
         
     ]
 });
